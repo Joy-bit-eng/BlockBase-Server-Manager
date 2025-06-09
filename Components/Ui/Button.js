6f95588c533f71996e35580bbe0b1444a336e7a9
Joy-bit-eng <ls9988236@gmail.com>
2025-06-09 11:14:54 +0530

    Create Button.js

---
+++ Components/Ui/Button.js
@@ -0,0 +1,11 @@
+// components/ui/button.js
+export function Button({ children, className = '', ...props }) {
+  return (
+    <button
+      className={`px-4 py-2 rounded font-semibold ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
