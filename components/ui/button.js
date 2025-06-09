528944a017deec52e3f561b23534d14818962190
Joy-bit-eng <ls9988236@gmail.com>
2025-06-09 13:25:24 +0530

    Update button.js

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,1 +1,11 @@
-
+// components/ui/button.js
+export default function Button({ children, ...props }) {
+  return (
+    <button
+      {...props}
+      className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition"
+    >
+      {children}
+    </button>
+  );
+}
